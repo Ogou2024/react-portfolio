936bfcf20bdfcf5e65c3ba732a5e6b3d882ed935
Ogou2024 <livenghtston.cius@gmail.com>
2024-06-24 22:26:35 -0400

    initialize

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,17 @@
+export function Contact() {
+    return (
+        <>
+            <h1>Contact Me</h1>
+            <p>Tel: +1 (407) 900 - 9198<br />
+            Available in Whatsapp</p>
+            <p>Email: lcius@konesanslakay.com</p>
+            
+            <p>Address:<br />
+                436 Road<br />
+                Altamonte Springs<br />
+                Florida 32714 <br />
+            </p>
+            <hr/>
+        </>
+    ) 
+}
